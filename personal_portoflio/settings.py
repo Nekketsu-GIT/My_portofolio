9c8b2b68db0fa7566278871f932f548bce9748ee
Nekketsu-GIT <54125039+Nekketsu-GIT@users.noreply.github.com>
2020-06-06 19:40:57 +0000

    update

--- personal_portoflio/settings.py
+++ personal_portoflio/settings.py
@@ -80,10 +80,14 @@ WSGI_APPLICATION = 'personal_portoflio.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+	'default': {
+		'ENGINE': 'django.db.backends.postgresql_psycopg2',
+		'NAME': 'my_portofolio',
+		'USER': 'postgres',
+		'PASSWORD': 'dacosta97',
+		'HOST': 'localhost',
+		'PORT': '5432',
+	}
 }
 
 # Password validation
